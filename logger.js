defb39b6c81783544e0f86f5edf580a13d834b04
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-20 15:12:28 -0700

    Create message queue client server in logger.js and have it subscribed to all possible events which emits custom unique console.logs per event

---
+++ logger.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Q = require('@nmq/q/client');
+ 
+const db = new Q('database');
+const files = new Q('files');
+
+db.subscribe('create', payload => {
+  console.log('CREATE: ', payload);
+});
+
+db.subscribe('read', payload => {
+  console.log('READ: ', payload);
+});
+
+db.subscribe('update', payload => {
+  console.log('UPDATE: ', payload);
+});
+
+db.subscribe('delete', payload => {
+  console.log('DELETE: ', payload);
+});
+
+db.subscribe('error', payload => {
+  console.log('ERROR: ', payload);
+});
+
+files.subscribe('save', payload => {
+  console.log('SAVE: ', payload);
+});
